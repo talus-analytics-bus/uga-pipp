fbe6b27f03622ecc771720fb0a7037ce1aa73175
David Rosado <dr997@georgetown.edu>
2023-12-04 14:02:34 -0500

    adding content range, response type and pagination

--- network/bv_brc_api.py
+++ network/bv_brc_api.py
@@ -5,6 +5,13 @@ from loguru import logger
 class BVBRC:
     async def _bv_brc_api(self):
         base_url = "https://patricbrc.org/api/"
+    async def _bv_brc_api(
+        self,
+        data_type,
+        count: int,
+        start: int,
+    ):
+        base_url = f"https://patricbrc.org/api/{data_type}/?http_content-range=99&http_accept=application/json&limit({count},{start})"
         async with aiohttp.ClientSession(trust_env=True) as session:
             async with session.get(base_url) as response:
                 return await response.json()
